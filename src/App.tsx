caa9e8d0e47ae7e2e3f1440ca6bbc0521720546f
Aditya Kumar <k.aditya00@gmail.com>
2020-12-22 20:46:08 +0530

    feat: Home, CharacterDetail empty pages
    - configure tslint to use jsx=react
    - install material-ui, react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Home from "./components/home";
+import CharacterDetails from "./components/character-details";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World! How are you?
+      <BrowserRouter>
+        <Switch>
+          <Route path="/character/:charId">
+            <CharacterDetails />
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/character-details/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'; 
+
+export default function CharacterDetails() {
+
+
+  return (
+    <>
+    Character Details Page
+    </>
+  )
+}
